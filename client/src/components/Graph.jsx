16574d3b738f92a6b667dcaa27529fa1c34336d2
kale007 <rajas.p.kale@gmail.com>
2017-05-10 11:23:53 -0700

    Renders graph onto webpage and merges with recent PR

--- client/src/components/Graph.jsx
+++ client/src/components/Graph.jsx
@@ -17,34 +17,47 @@ class Graph extends React.Component {
     this.attributes;
   }
 
+  // When the DOM is ready, create the chart.
+  componentDidMount () {
+      // Extend Highcharts with modules
+      if (this.props.modules) {
+          this.props.modules.forEach(function (module) {
+              module(Highcharts);
+          });
+      }
+      // Set container which the chart should render to.
+      this.chart = new Highcharts[this.props.type || "Chart"](
+          'container',
+          options
+      );
+
+      // Figure out how to set theme into chart. May not work here.
+      // this.chart.setOptions(theme);
+  }
+
+  //Destroy chart before unmount.
+  componentWillUnmount () {
+      this.chart.destroy();
+  }
+
   componentWillReceiveProps(props) {
     if (props.state.entries.length > 0) {
       this.attributes = JSON.stringify(props.state.entries);
     }
   }
 
-  //Create the div which the chart will be rendered to.
   render() {
-    return (
-      <div>
-        GRAPHS HERE
-        <br/>
-        {this.attributes}
-      </div>
-    )
+      return React.createElement('div', { id: 'container' });
   }
-
-  // render() {
-  //   return (
-  //     <div id="container">
-  //       GRAPHS HERE
-  //       <br />
-  //       <script type="text/javascript" src="../graphGen/highCharts1.js"></script>
-  //       <script type="text/javascript" src="../graphGen/highChartsTheme.js"></script>
-  //
-  //     </div>
-  //   )
-  // }
+//   render() {
+//     return (
+//       <div id="container">
+//         GRAPHS HERE
+//         <br/>
+//         {this.attributes}
+//       </div>
+//     )
+//   }
 }
 
 export default Graph;
