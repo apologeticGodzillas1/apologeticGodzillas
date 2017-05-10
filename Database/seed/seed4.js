c7b3f612109a5dc2c0a33deb56e2896bad440da3
kale007 <rajas.p.kale@gmail.com>
2017-05-09 18:43:46 -0700

    Creates a random data generator file and populates dummy data

---
+++ Database/seed/seed4.js
@@ -0,0 +1,13 @@
+// data produced with randDataGen(100, 3)
+// most null data
+
+var series = [{
+  name: 'mind',
+  data: [0,7,6,7,null,null,null,null,null,null,null,8,4,null,null,9,null,9,null,null,null,null,null,9,7,3,1,5,null,3,null,null,null,null,null,null,9,6,8,null,1,null,6,8,null,2,null,8,null,null,null,null,null,null,null,null,null,null,null,3,null,7,null,8,7,7,null,null,null,null,null,null,null,null,3,6,null,8,null,null,null,null,null,7,null,null,null,null,8,8,null,null,null,null,null,3,6,null,8,null,2]
+}, {
+  name: 'body',
+  data: [0,10,8,10,null,null,null,null,null,null,null,7,5,null,null,2,null,4,null,null,null,null,null,7,3,7,1,2,null,7,null,null,null,null,null,null,2,4,7,null,9,null,6,4,null,10,null,8,null,null,null,null,null,null,null,null,null,null,null,1,null,9,null,6,9,2,null,null,null,null,null,null,null,null,9,3,null,9,null,null,null,null,null,8,null,null,null,null,4,8,null,null,null,null,null,10,8,null,5,null,9]
+}, {
+  name: 'spoil',
+  data: [0,7,8,1,null,null,null,null,null,null,null,4,8,null,null,4,null,6,null,null,null,null,null,5,5,3,2,9,null,2,null,null,null,null,null,null,9,2,10,null,5,null,10,6,null,1,null,6,null,null,null,null,null,null,null,null,null,null,null,9,null,1,null,10,5,9,null,null,null,null,null,null,null,null,10,5,null,8,null,null,null,null,null,8,null,null,null,null,8,9,null,null,null,null,null,4,3,null,8,null,1]
+}]
