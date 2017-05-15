99bf65b62674e9310870e933554651ad10752b4a
Jon Eric Escobedo <jonericescobedo@gmail.com>
2017-05-15 14:56:45 -0700

    (front-end) Fix all merge conflicts for front-end changes

--- client/src/components/CategoryList.jsx
+++ client/src/components/CategoryList.jsx
@@ -9,47 +9,70 @@ class CategoryList extends React.Component {
   render() {
     return (
       <div>
-        <div>
-          <form className="get-data-form">
-            <div className="form-group">
-              <label htmlFor="nameInput">Username</label>
-              <input type="text" className="form-control" id="nameInput" placeholder="Your name / ID" value={this.props.state.username} onChange={this.props.handleChangeUsername}></input>
+        <div className="row custom-height">
+        <div className="col-12">
+        <div className="mind card custom-card">
+          <div className="card-block">
+          <p className="lead">mind</p>
+          <form>
+            <div className="form-group row col-12">
+              <input type="text" className="form-control form-control-sm" id="mindInput1" placeholder="Hours Slept (0-10)" value={this.props.state.mindInput1} onChange={this.props.handleChangeMind1}/>
+            </div>
+            <div className="form-group row col-12">
+              <input type="text" className="form-control form-control-sm" id="mindInput2" placeholder="Reading in 10 min (0-10)" value={this.props.state.mindInput2} onChange={this.props.handleChangeMind2}/>
+            </div>
+            <div className="form-group row col-12">
+              <input type="text" className="form-control form-control-sm" id="mindInput3" placeholder="Learning a Skill (0-10)" value={this.props.state.mindInput3} onChange={this.props.handleChangeMind3}/>
             </div>
           </form>
-          <button type="submit" className="btn btn-primary" onClick={this.props.getInfoClick}>Get Info</button>
+          </div>
         </div>
-        <div>
-          <form className="new-form">
-            <div className="form-group">
-              <label htmlFor="nameInput">Name</label>
-              <input type="text" className="form-control" id="nameInput" placeholder="Your name / ID" value={this.props.state.name} onChange={this.props.handleChangeName}></input>
-            </div>
+        </div>
+        </div>
 
-            <div className="form-group">
-              <label htmlFor="mindInput1">Mind</label>
-              <input type="text" className="form-control" id="mindInput1" placeholder="Hours Slept (0-10)" value={this.props.state.mindInput1} onChange={this.props.handleChangeMind1}></input>
-              <input type="text" className="form-control" id="mindInput2" placeholder="Reading in 10 min (0-10)" value={this.props.state.mindInput2} onChange={this.props.handleChangeMind2}></input>
-              <input type="text" className="form-control" id="mindInput3" placeholder="Learning a Skill (0-10)" value={this.props.state.mindInput3} onChange={this.props.handleChangeMind3}></input>
+        <div className="row custom-height">
+        <div className="col-12">
+        <div className="body card custom-card">
+          <div className="card-block">
+          <p className="lead">body</p>
+          <form>
+            <div className="form-group row col-12">
+              <input type="text" className="form-control form-control-sm" id="bodyInput1" placeholder="Exercise in 10 min (0-10)" value={this.props.state.bodyInput1} onChange={this.props.handleChangeBody1}/>
             </div>
-
-            <div className="form-group">
-              <label htmlFor="bodyInput">Body</label>
-              <input type="text" className="form-control" id="bodyInput1" placeholder="Exercise in 10 min (0-10)" value={this.props.state.bodyInput1} onChange={this.props.handleChangeBody1}></input>
-              <input type="text" className="form-control" id="bodyInput2" placeholder="Cups of Water (0-10)" value={this.props.state.bodyInput2} onChange={this.props.handleChangeBody2}></input>
-              <input type="text" className="form-control" id="bodyInput3" placeholder="Servings of Fruits and Veggies (0-10)" value={this.props.state.bodyInput3} onChange={this.props.handleChangeBody3}></input>
+            <div className="form-group row col-12">
+              <input type="text" className="form-control form-control-sm" id="bodyInput2" placeholder="Cups of Water (0-10)" value={this.props.state.bodyInput2} onChange={this.props.handleChangeBody2}/>
             </div>
-
-            <div className="form-group">
-              <label htmlFor="soulInput">Soul</label>
-              <input type="text" className="form-control" id="soulInput1" placeholder="Mood Scale (0-10)" value={this.props.state.soulInput1} onChange={this.props.handleChangeSoul1}></input>
-              <input type="text" className="form-control" id="soulInput2" placeholder="Yoga/Meditation in 5 min (0-10)" value={this.props.state.soulInput2} onChange={this.props.handleChangeSoul2}></input>
-              <input type="text" className="form-control" id="soulInput3" placeholder="# of Positive Social Interations (0-10)" value={this.props.state.soulInput3} onChange={this.props.handleChangeSoul3}></input>
+            <div className="form-group row col-12">
+              <input type="text" className="form-control form-control-sm" id="bodyInput3" placeholder="Servings of Fruits and Veggies (0-10)" value={this.props.state.bodyInput3} onChange={this.props.handleChangeBody3}/>
             </div>
-
           </form>
-          <button type="submit" className="btn btn-primary" onClick={this.props.handleClick}>Submit Info</button>
-
+          </div>
         </div>
+        </div>
+        </div>
+
+        <div className="row custom-height">
+        <div className="col-12">
+        <div className="soul card custom-card">
+          <div className="card-block">
+          <p className="lead">soul</p>
+          <form>
+            <div className="form-group row col-12">
+              <input type="text" className="form-control form-control-sm" id="soulInput1" placeholder="Mood Scale (0-10)" value={this.props.state.soulInput1} onChange={this.props.handleChangeSoul1}/>
+            </div>
+            <div className="form-group row col-12">
+              <input type="text" className="form-control form-control-sm" id="soulInput2" placeholder="Yoga/Meditation in 5 min (0-10)" value={this.props.state.soulInput2} onChange={this.props.handleChangeSoul2}/>
+            </div>
+            <div className="form-group row col-12">
+              <input type="text" className="form-control form-control-sm" id="soulInput3" placeholder="# of Positive Social Interations (0-10)" value={this.props.state.soulInput3} onChange={this.props.handleChangeSoul3}/>
+            </div>
+          </form>
+          <button type="submit" className="btn btn-secondary" onClick={this.props.handleClick}>Submit Info</button>
+          </div>
+        </div>
+        </div>
+        </div>
+
       </div>
     )
   }
---
+++ client/src/components/Login.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Login extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return(
+      <div>
+        <div className="container vertical-center">
+          <div>
+          <h1 className="display-1 hero">Valence.</h1>
+          <p className="text-muted hero-secondary">(va·lence)</p>
+          
+          <div className="row">
+            <div className="col-4">
+              <p className="text-muted definition">
+              the intrinsic "good"-ness (positive valence) or 
+              "bad"-ness (negative valence) of an event, object, 
+              or situation.
+              </p>
+              <h4 id="slogan">Take <span id="charge">charge</span> of your life.</h4>
+            </div>
+          
+            <div className="col login-field">
+              <label className="login" htmlFor="nameInput">Enter your name:</label>
+              <div className="input-group login">
+              <input type="text" className="form-control form-control-sm col-4" id="nameInput" placeholder="Your name" value={this.props.state.username} onChange={this.props.handleChangeUsername}></input>
+              <span><button type="submit" className="btn btn-secondary btn-sm" onClick={this.props.getInfoClick}>Go!</button></span>
+              </div>
+              <p className="login">{this.props.state.userNotFound === true ? 'User not found. Try again.' : ''}</p>
+            </div>
+          </div>
+          </div>
+
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Login;
